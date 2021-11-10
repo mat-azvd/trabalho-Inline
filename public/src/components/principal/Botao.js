01ea0563ecbcd9d900310c71d88ee35251e72d1f
mat.azvd <mat.azvd@gmail.com>
2021-11-10 02:21:41 -0300

    scroll

--- public/src/components/principal/Botao.js
+++ public/src/components/principal/Botao.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './../../App.css';
-
 const Botao = (props) => ( 
     < button id="botao" onClick={props.onClick} > {props.label} </button>
 )
+
 export default Botao
