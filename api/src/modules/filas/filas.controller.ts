b0fd04e5b1d91f04c29db19705f9ae26bd5eae1e
Thiago Nogueira <thiago.nogueira187@gmail.com>
2021-11-10 00:35:38 -0500

    fix: Corrigido documentação

--- api/src/modules/filas/filas.controller.ts
+++ api/src/modules/filas/filas.controller.ts
@@ -1,10 +1,13 @@
 import { Body, Controller, Delete, Get, Param, Post, Put, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { UsuarioPayload } from '../auth/dto/login.dto';
 import { LocalAuthGuard } from '../auth/guards/local-auth.guard';
 import { CurrentUser } from '../auth/strategies/jwt.strategy';
 import { FilaDto } from './dto/fila.dto';
 import { FilasService } from './filas.service';
 
+@ApiTags('filas')
+@ApiBearerAuth()
 @Controller('filas')
 export class FilasController {
   constructor(private readonly filasService: FilasService) {}
