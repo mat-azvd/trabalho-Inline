c0ba8d03cb826c075b79fd302691d19d668651ba
Thiago Nogueira de Carvalho <thiago.nogueira@cubos.io>
2021-11-19 03:12:22 +0000

    fix: Corrigido cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -6,7 +6,7 @@ const APP_NAME = process.env.npm_package_name;
 const APP_VERSION = process.env.npm_package_version;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   const config = new DocumentBuilder()
     .setTitle(APP_NAME)
     .setDescription(`The ${APP_NAME} API description`)
