55f43fafaa4d43482f5575bcee1a180ccdfa20d1
Helena Oliveira <helena.oliveira@hypeflame.com.br>
2021-11-22 17:32:55 -0300

    fix: ajuste de redirecionamento sidebar

--- public/src/components/Sidebar/Sidebar.js
+++ public/src/components/Sidebar/Sidebar.js
@@ -3,81 +3,91 @@ import Setup from "../../pages/Setup";
 import ListaDeFilasAdm from "../../pages/ListaDeFilasAdm";
 
 import {
-    Icon,
-    CloseIcon,
-    SidebarContainer,
-    SidebarWrapper,
-    SidebarMenu,
-    SidebarLink,
-    SideBtnWrap,
-    SidebarRoute,
+  Icon,
+  CloseIcon,
+  SidebarContainer,
+  SidebarWrapper,
+  SidebarMenu,
+  SidebarLink,
+  SideBtnWrap,
+  SidebarRoute,
 } from "./SidebarElements";
 import { useHistory } from "react-router-dom";
-import userService from '../../services/user';
+import userService from "../../services/user";
+import CreateQueue from "../CreateQueue/CreateQueue";
+import StoreRegister from "../StoreRegister/StoreRegister";
 
 const Sidebar = ({ isOpen, toggle }) => {
-    const history = useHistory();
-    const [isPermitted, setIsPermitted] = useState(false);
-    const token = window.localStorage.getItem("token");
+  const history = useHistory();
+  const [isPermitted, setIsPermitted] = useState(false);
+  const token = window.localStorage.getItem("token");
 
-    useEffect(() => {
-        getUser()
-    }, [])
+  useEffect(() => {
+    getUser();
+  }, []);
 
-    async function getUser() {
-        if (!token) {
-            return setIsPermitted(false);
-        }
-
-        const user = await userService.getLoggedUser();
-
-        setIsPermitted(Boolean(user.lojaId))
+  async function getUser() {
+    if (!token) {
+      return setIsPermitted(false);
     }
 
-    function hasPermisison () {
-        if (isPermitted) {
-            return (
-                <SidebarLink href="/queue-manager" onClick={ListaDeFilasAdm}>
-                    Gerenciar Filas
-                </SidebarLink>
-            )
-        }
+    const user = await userService.getLoggedUser();
+
+    setIsPermitted(Boolean(user.lojaId));
+  }
+
+  function hasPermisison() {
+    if (isPermitted) {
+      return (
+        <>
+          <SidebarLink href="/store-register" onClick={StoreRegister}>
+            Login
+          </SidebarLink>
+          <SidebarLink href="/create-queue" onClick={CreateQueue}>
+            Criar Fila
+          </SidebarLink>
+          <SidebarLink href="/queue-manager" onClick={ListaDeFilasAdm}>
+            Gerenciar Filas
+          </SidebarLink>
+        </>
+      );
     }
+  }
 
-    function exit() {
-        localStorage.clear();
-        history.push(`/`);
+  function exit() {
+    localStorage.clear();
+    history.push(`/`);
+  }
+
+  function isLogged() {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      return (
+        <SideBtnWrap>
+          <SidebarRoute onClick={() => exit()}>Sair</SidebarRoute>
+        </SideBtnWrap>
+      );
     }
+  }
 
-    function isLogged() {
-        const token = localStorage.getItem('token');
+  return (
+    <SidebarContainer isOpen={isOpen} onClick={toggle}>
+      <Icon onClick={toggle}>
+        <CloseIcon />
+      </Icon>
+      <SidebarWrapper>
+        <SidebarMenu>
+          {hasPermisison()}
+          <SidebarLink href="/setup-screen" onClick={Setup}>
+            Configurações
+          </SidebarLink>
+        </SidebarMenu>
 
-        if (token) {
-            return (
-                <SideBtnWrap>
-                    <SidebarRoute onClick={() => exit()}>Sair</SidebarRoute>
-                </SideBtnWrap>
-            )
-        }
-    }
-
-    return (
-        <SidebarContainer isOpen={isOpen} onClick={toggle}>
-            <Icon onClick={toggle}>
-                <CloseIcon />
-            </Icon>
-            <SidebarWrapper>
-                <SidebarMenu>
-                    {hasPermisison()}
-                    <SidebarLink href="/setup-screen" onClick={Setup}>
-                        Configurações
-                    </SidebarLink>
-                </SidebarMenu>
-
-                {isLogged()}
-            </SidebarWrapper>
-        </SidebarContainer>
-    );
+        {isLogged()}
+      </SidebarWrapper>
+    </SidebarContainer>
+  );
 };
 
 export default Sidebar;
